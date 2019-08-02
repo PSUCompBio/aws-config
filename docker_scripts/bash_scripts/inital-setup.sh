7735d4b520778d186056b576636a7568e2e1f86f
Reuben Kraft <reuben.kraft@gmail.com>
2019-08-02 15:38:47 -0400

    updates for building paraview

--- docker_scripts/bash_scripts/inital-setup.sh
+++ docker_scripts/bash_scripts/inital-setup.sh
@@ -43,18 +43,20 @@ if [ "$INSTANCE" == "ami" ]; then
   sudo yum groupinstall -y  "Development Tools"
 elif [ "$INSTANCE" == "ubuntu" ]; then
         apt-get update -y
-        apt-get install -y libgl1 libxt6 build-essential cmake-curses-gui xvfb nodejs npm freeglut3-dev libblas-dev liblapack-dev openmpi-bin openmpi-common libopenmpi-dev python3-pip autotools-dev automake libpcre3-dev bison byacc libfreetype6-dev pkg-config libfontconfig1-dev mdm
+        apt-get install -y libgl1 libxt6 build-essential cmake-curses-gui xvfb
+        apt-get install -y nodejs npm freeglut3-dev libblas-dev liblapack-dev
+        apt-get install -y openmpi-bin openmpi-common libopenmpi-dev python3-pip
+        apt-get install -y autotools-dev automake libpcre3-dev bison byacc
+        apt-get install -y libfreetype6-dev pkg-config libfontconfig1-dev mdm
         apt-get install -y libxt6
         apt-get install -y wget
-        #apt-get install -y qt5-default
-        #apt-get install -y qt5-doc
-        #apt-get install -y libqt5x11extras5-dev
         apt-get install -y llvm
         apt-get install -y libclang-dev
         apt-get install -y libqt5x11extras5-dev
         apt-get install -y qttools5-dev
         apt-get install -y qtcreator
         apt-get install -y libosmesa6 libosmesa6-dev
+        apt-get install -y libegl1-mesa libegl1-mesa-dev libgl1-mesa-dev libgles2-mesa-dev libglu1-mesa-dev mesa-common-dev  libglw1-mesa-dev mesa-utils  mesa-utils-extra
         # sudo apt-get install -y libtk8.6
         npm i -g npx
         pip3 install numpy matplotlib
