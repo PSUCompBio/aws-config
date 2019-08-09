ab3845272279afe380a2e49d07f66575df4b5ba3
Reuben Kraft <reuben.kraft@gmail.com>
2019-08-09 09:00:04 -0400

    Update install-paraview-image-write.sh

--- docker_scripts/bash_scripts/install-paraview-image-write.sh
+++ docker_scripts/bash_scripts/install-paraview-image-write.sh
@@ -1,7 +1,7 @@
 cd /home/$UNAMEX
 git clone https://github.com/PSUCompBio/paraview-image-write
 cd paraview-image-write
-xvfb-run --server-args="-screen 0 1024x768x24" pvpython test.py
+xvfb-run -a --server-args="-screen 0 1024x768x24" pvpython ppr1.py
 echo " -------------------------------------------------------------------"
 echo " ----------    Complete Paraview Image Write Test             ------"
 echo " -------------------------------------------------------------------"
