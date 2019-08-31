0101acff581e9d18e8d5a0d34e9f6897148714a0
Reuben Kraft <reuben.kraft@gmail.com>
2019-08-31 12:06:54 -0400

    added oce script

---
+++ docker_scripts_ami/bash_scripts/install-oce.sh
@@ -0,0 +1,32 @@
+cd /home/$UNAMEX
+#git clone https://github.com/tpaviot/oce.git
+git clone https://github.com/PSUCompBio/oce.git
+echo "-------------OCE CLONE FINISH TIME--------------------"
+date
+echo "-----------------------------------"
+cd oce
+mkdir build
+cd build
+echo "------------python oce make start---------------------"
+date
+echo "-----------------------------------"
+cmake ..
+make -j8
+make install
+echo "-------------python oce make end--------------------"
+date
+echo "-----------------------------------"
+echo "PATH=\$PATH:/usr/local/bin" >> /home/$UNAMEX/.bash_profile
+echo "export PATH" >> /home/$UNAMEX/.bash_profile
+# note: the variable $LD_LIBRARY_PATH was not being recognized by Docker so
+# here RK removed it. Ideally we can figure out ow to use these variables and turn
+# back on.
+#echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib" >> /home/$UNAMEX/.bash_profile
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib" >> /home/$UNAMEX/.bash_profile
+source /home/$UNAMEX/.bash_profile
+echo " -------------------------------------------------------------------"
+echo " ----------               Finished Installing OCE             ------"
+echo " -------------------------------------------------------------------"
+echo "-----------------OCE END TIME----------------"
+date
+echo "-----------------------------------"
