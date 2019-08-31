7837e2bae17ea656616e66b3b35a8aad2194e8a2
Reuben Kraft <reuben.kraft@gmail.com>
2019-08-31 06:22:52 -0400

    initial ami-setup

--- create_docker_image.sh
+++ create_docker_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-INSTANCE="ubuntu"
+INSTANCE_TYPE="ubuntu"
 #
 
 echo "SCRIPT START TIME"
@@ -9,14 +9,20 @@ date
 echo " -------------------------------------------------------------------"
 echo " ----------                Instance Type                      ------"
 echo " -------------------------------------------------------------------"
-echo $INSTANCE
+echo $INSTANCE_TYPE
 echo " -------------------------------------------------------------------"
 echo " -------------------------------------------------------------------"
-if [ "$INSTANCE" == "ami" ]; then
+if [ "$INSTANCE_TYPE" == "ami" ]; then
         UNAMEX="ec2-user"
-elif [ "$INSTANCE" == "ubuntu" ]; then
+        HOME="/home/ec2-user"
+        sudo yum update -y
+        sudo yum install docker -y
+        sudo yum install git -y
+elif [ "$INSTANCE_TYPE" == "ubuntu" ]; then
         UNAMEX="ubuntu"
         HOME="/home/ubuntu"
+        sudo apt-get update
+        sudo apt-get install docker.io -y
 else
         echo "Instance Type does not exist in config_bash script - please update it."
 fi
@@ -28,15 +34,13 @@ echo " -------------------------------------------------------------------"
 echo " -------------------------------------------------------------------"
 cd /home/$UNAMEX
 
-sudo apt-get update
-sudo apt-get install docker.io -y
-systemctl start docker
-systemctl enable docker
+sudo systemctl start docker
+sudo systemctl enable docker
 # clone specific branch
-#git clone -b minor-fixes https://github.com/PSUCompBio/aws-config.git
+git clone -b ami-setup https://github.com/PSUCompBio/aws-config.git
 # clone master branch
-git clone https://github.com/PSUCompBio/aws-config.git
-cd aws-config/docker_scripts
-docker build -t nsfcareer .
-cd /home/$UNAMEX
-sudo chown -R ubuntu *
+#git clone https://github.com/PSUCompBio/aws-config.git
+cd aws-config/docker_scripts_$INSTANCE_TYPE
+#docker build -t nsfcareer .
+#cd /home/$UNAMEX
+#sudo chown -R ubuntu *
