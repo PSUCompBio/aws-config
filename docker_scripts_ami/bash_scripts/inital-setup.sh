d662ffbf2e326a3387a7a483c8be49725dc69182
Reuben Kraft <reuben.kraft@gmail.com>
2019-08-31 09:30:42 -0400

    Update inital-setup.sh

--- docker_scripts_ami/bash_scripts/inital-setup.sh
+++ docker_scripts_ami/bash_scripts/inital-setup.sh
@@ -3,20 +3,18 @@
 cd /home/$UNAMEX
 echo "Now in /home/$UNAMEX"
 
-sudo yum install -y mesa-libGL
-sudo yum install -y mesa-libGL-devel
-sudo yum install -y mesa-libGLES
-sudo yum install -y mesa-libGLES-devel
-sudo yum install -y xorg-x11-server-Xvfb
-sudo yum groupinstall -y  "Development Tools"}
+yum install -y mesa-libGL
+yum install -y mesa-libGL-devel
+yum install -y mesa-libGLES
+yum install -y mesa-libGLES-devel
+yum install -y xorg-x11-server-Xvfb
 
 echo "alias l='ls -ltr' " >> /home/$UNAMEX/.bash_profile
 echo "alias h='history' " >> /home/$UNAMEX/.bash_profile
 echo "alias python='python3' " >> /home/$UNAMEX/.bash_profile
 source /home/$UNAMEX/.bash_profile
-sleep 5
-/usr/bin/ncpus
-NCPUS_VAR=$(ncpus)
+nproc
+NCPUS_VAR=$(nproc)
 echo "ncpus = $NCPUS_VAR"
 echo " -------------------------------------------------------------------"
 echo " ----------       Finished Installing Essential Dev Tools     ------"
