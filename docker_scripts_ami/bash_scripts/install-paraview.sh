adb9fda7b1ebda6bf234d4024c42477d51e4e9b0
Reuben Kraft <reuben.kraft@gmail.com>
2019-09-22 19:56:26 -0400

    updates

--- docker_scripts_ami/bash_scripts/install-paraview.sh
+++ docker_scripts_ami/bash_scripts/install-paraview.sh
@@ -9,8 +9,9 @@ mv ParaView-5.7.0-RC4-osmesa-MPI-Linux-Python3.7-64bit Paraview-5.7.0
 #git submodule update --init --recursive
 #mkdir build
 #cd build
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/$UNAMEX/Paraview-5.7.0/lib
-PATH=$PATH:/home/$UNAMEX/Paraview-5.7.0/bin
+echo "LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/home/$UNAMEX/Paraview-5.7.0/lib" >> /home/$UNAMEX/.bash_profile
+#PATH=$PATH:/home/$UNAMEX/Paraview-5.7.0/bin
+echo "PATH=\$PATH:/home/$UNAMEX/Paraview-5.7.0/bin" >> /home/$UNAMEX/.bash_profile
 #cmake ../ -DCMAKE_BUILD_TYPE=Release -DPARAVIEW_ENABLE_PYTHON=ON -DVTK_USE_X=OFF -DVTK_OPENGL_HAS_OSMESA=ON -DPARAVIEW_BUILD_QT_GUI=OFF -DCMAKE_CXX_FLAGS="-lglut -lGLU"
 #cmake ../ -DCMAKE_BUILD_TYPE=Release -DPARAVIEW_ENABLE_PYTHON=ON -DVTK_USE_X=OFF -DVTK_OPENGL_HAS_OSMESA=ON -DPARAVIEW_BUILD_QT_GUI=OFF -DCMAKE_CXX_FLAGS="-L/usr/lib/x86_64-linux-gnu/libGL.so  -L/usr/lib/x86_64-linux-gnu/libGLU.so-L/usr/lib/x86_64-linux-gnu/libOSMesa.so"
 #cmake ../ -DCMAKE_BUILD_TYPE=Release -DPARAVIEW_ENABLE_PYTHON=ON -DVTK_USE_X=OFF -DVTK_OPENGL_HAS_OSMESA=ON -DPARAVIEW_BUILD_QT_GUI=OFF -DCMAKE_CXX_FLAGS="-L/usr/lib/x86_64-linux-gnu/libGL.so  -L/usr/lib/x86_64-linux-gnu/libGLU.so-L/usr/lib/x86_64-linux-gnu/libOSMesa.so" -DVTK_USE_X=ON
