2f55b65b6d8dc0cf6b467a5a1b46145bf64e631e
Reuben Kraft <reuben.kraft@gmail.com>
2019-09-23 16:34:44 -0400

    Update inital-setup.sh

--- docker_scripts_ami/bash_scripts/inital-setup.sh
+++ docker_scripts_ami/bash_scripts/inital-setup.sh
@@ -3,14 +3,14 @@
 cd /home/$UNAMEX
 echo "Now in /home/$UNAMEX"
 yum install -y wget
-yum install -y mesa-libGL
-yum install -y mesa-libGL-devel
-yum install -y mesa-libGLES
-yum install -y mesa-libGLES-devel
+#yum install -y mesa-libGL
+#yum install -y mesa-libGL-devel
+#yum install -y mesa-libGLES
+#yum install -y mesa-libGLES-devel
 yum install -y xorg-x11-server-Xvfb
-yum install -y libXt-devel
-yum install -y  mesa-libOSMesa mesa-libOSMesa-devel
-yum install -y mesa-libEGL-devel
+#yum install -y libXt-devel
+#yum install -y  mesa-libOSMesa mesa-libOSMesa-devel
+#yum install -y mesa-libEGL-devel
 yum install -y openmpi openmpi-devel
 echo "alias l='ls -ltr' " >> /home/$UNAMEX/.bash_profile
 echo "alias h='history' " >> /home/$UNAMEX/.bash_profile
