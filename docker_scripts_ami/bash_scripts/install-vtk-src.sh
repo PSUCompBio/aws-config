15c5636465f4385e0a221ae327a350501d40d53b
Reuben Kraft <reuben.kraft@gmail.com>
2019-09-24 18:33:45 -0400

    Update install-vtk-src.sh

--- docker_scripts_ami/bash_scripts/install-vtk-src.sh
+++ docker_scripts_ami/bash_scripts/install-vtk-src.sh
@@ -1,10 +1,12 @@
 cd /home/$UNAMEX
 git clone https://github.com/Kitware/VTK
-wget https://gitlab.kitware.com/vtk/vtk/-/archive/v8.2.0/vtk-v8.2.0.tar.gz
-gunzip vtk-v8.2.0.tar.gz
-tar -xvf vtk-v8.2.0.tar
-rm vtk-v8.2.0.tar
-export VTK_ROOT=/home/$UNAMEX/vtk-v8.2.0
+#wget https://gitlab.kitware.com/vtk/vtk/-/archive/v8.2.0/vtk-v8.2.0.tar.gz
+#wget https://gitlab.kitware.com/vtk/vtk/-/archive/v5.10.1/vtk-v5.10.1.tar.gz
+wget https://gitlab.kitware.com/vtk/vtk/-/archive/v6.3.0/vtk-v6.3.0.tar.gz
+gunzip vtk-v6.3.0.tar.gz
+tar -xvf vtk-v6.3.0.tar
+rm vtk-v6.3.0.tar
+export VTK_ROOT=/home/$UNAMEX/vtk-v6.3.0
 cd $VTK_ROOT
 mkdir build
 cd build
@@ -15,7 +17,7 @@ yum install -y mesa-libGLES-devel
 yum install -y  mesa-libOSMesa mesa-libOSMesa-devel
 yum install -y mesa-libEGL-devel
 #cmake3 .. -DVTK_PYTHON_VERSION=3 -DPYTHON_EXECUTABLE="/usr/bin/python3"
-cmake3 .. -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release  -DVTK_OPENGL_HAS_EGL=OFF  -DVTK_OPENGL_HAS_OSMESA=OFF -DVTK_OPENGL_USE_GLES=OFF -DVTK_USE_X=OFF  -DVTK_WRAP_PYTHON=ON  -DBUILD_EXAMPLES=OFF -DVTK_PYTHON_VERSION=3 -DPYTHON_EXECUTABLE="/usr/bin/python3" -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF -DCMAKE_CXX_FLAGS="-lGLU -lGL"
+cmake3 .. -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release  -DVTK_OPENGL_HAS_EGL=OFF  -DVTK_OPENGL_HAS_OSMESA=OFF -DVTK_OPENGL_USE_GLES=OFF -DVTK_USE_X=OFF  -DVTK_WRAP_PYTHON=ON  -DBUILD_EXAMPLES=OFF -DVTK_PYTHON_VERSION=3 -DPYTHON_EXECUTABLE="/usr/bin/python3" -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF -DCMAKE_CXX_FLAGS="-lGLU -lGL" -DPYTHON_INCLUDE_DIR=$(python3 -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")  -DPYTHON_LIBRARY=$(python3 -c "import distutils.sysconfig as sysconfig; print(sysconfig.get_config_var('LIBDIR'))")
 #MESA_INSTALL_PREFIX=/opt/osmesa_llvmpipe
 #yum install python2-devel -y
 #cmake3     -DBUILD_SHARED_LIBS=ON     -DVTK_WRAP_PYTHON=ON     -DVTK_USE_X=OFF     -DVTK_USE_SYSTEM_HDF5=ON        -DOPENGL_INCLUDE_DIR=${MESA_INSTALL_PREFIX}/include     -DOPENGL_gl_LIBRARY=${MESA_INSTALL_PREFIX}/lib/libGLU.so     -DOPENGL_glu_LIBRARY=${MESA_INSTALL_PREFIX}/lib/libglapi.so     -DVTK_OPENGL_HAS_OSMESA=ON     -DOSMESA_INCLUDE_DIR=${MESA_INSTALL_PREFIX}/include     -DOSMESA_LIBRARY=${MESA_INSTALL_PREFIX}/lib/libOSMesa.so ..
@@ -24,7 +26,7 @@ make -j $NCPUS_VAR
 make install
 cd /home/$UNAMEX
 echo "#VTK_ROOT written from inital-vtk-src.sh" >> /home/$UNAMEX/.bash_profile
-echo "VTK_ROOT=/home/$UNAMEX/vtk-v8.2.0" >> /home/$UNAMEX/.bash_profile
+echo "VTK_ROOT=/home/$UNAMEX/vtk-v6.3.0" >> /home/$UNAMEX/.bash_profile
 echo "#LD_LIBRARY_PATH written from inital-vtk-src.sh" >> /home/$UNAMEX/.bash_profile
 echo "LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:\$VTK_ROOT/build/bin:\$VTK_ROOT/build/lib" >> /home/$UNAMEX/.bash_profile
 echo " " >> /home/$UNAMEX/.bash_profile
