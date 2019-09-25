ca72d73a399fd5ce3c9b8357368ce5dd4c654ee7
Reuben Kraft <reuben.kraft@gmail.com>
2019-09-25 06:25:22 -0400

    updated for vtk build

--- docker_scripts_ami/bash_scripts/install-MergePolyData.sh
+++ docker_scripts_ami/bash_scripts/install-MergePolyData.sh
@@ -5,8 +5,10 @@ git clone https://github.com/PSUCompBio/MergePolyData
 cd MergePolyData
 mkdir build
 cd build
-cmake3 .. -DVTK_DIR=$VTK_ROOT/build -DCMAKE_CXX_FLAGS=-I/usr/local/include/vtk-8.2
+cmake3 .. -DVTK_DIR=$VTK_ROOT/build -DCMAKE_CXX_FLAGS=-I/usr/local/include/vtk-8.0
 make -j $NCPUS_VAR
+./MergePolyData -in examples/4-Simple-Parts/part1.ply.vtk examples/4-Simple-Parts/part2.ply.vtk examples/4-Simple-Parts/part3.ply.vtk examples/4-Simple-Parts/part4.ply.vtk -out examples/4-Simple-Parts/combined.vtk -abaqus
+./Quad2Hex -in examples/convert-Quad2Hex/chank.ply -out examples/convert-Quad2Hex/chank_Hex.vtk
 xvfb-run -a --server-args="-screen 0 1024x768x24" ./ImageCapture examples/ply-to-png/model.ply  examples/ply-to-png/model.jpg examples/ply-to-png/test.png
 cd /home/$UNAMEX/MergePolyData
 /bin/bash pass_check.sh
