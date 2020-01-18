be256aac3eb996fd80f3284bf381b4c3a5b7af65
Reuben Kraft <reuben.kraft@gmail.com>
2020-01-17 19:34:08 -0500

    Update install-simple-nodejs-app.sh

--- docker_scripts_ami/bash_scripts/install-simple-nodejs-app.sh
+++ docker_scripts_ami/bash_scripts/install-simple-nodejs-app.sh
@@ -1,3 +1,5 @@
 git clone https://github.com/PSUCompBio/simple-nodejs-app
 cd simple-nodejs-app
+https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
+sudo yum -y install yarn nodejs
 node index.js
