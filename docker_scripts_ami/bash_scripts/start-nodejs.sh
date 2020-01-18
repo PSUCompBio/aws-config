078b3025d53ed9cb9da3b2dc81b7cac3b974354c
Reuben Kraft <reuben.kraft@gmail.com>
2020-01-18 12:03:00 -0500

    Update start-nodejs.sh

--- docker_scripts_ami/bash_scripts/start-nodejs.sh
+++ docker_scripts_ami/bash_scripts/start-nodejs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-forever start -a --uid simple_nodejs_app /var/www/simple_nodejs_app/index.js
- echo 'Success running the startup script for simple-nodejs_app!'
+forever start -a --uid simple_nodejs_app /var/www/simple-nodejs-app/index.js
+ echo 'Success running the startup script for simple-nodejs-app!'
