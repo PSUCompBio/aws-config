abec7e77439e4b5dfc739a86c196258d7ad772e5
Reuben Kraft <reuben.kraft@gmail.com>
2020-01-18 09:07:45 -0500

    node js updates

--- docker_scripts_ami/bash_scripts/start-nodejs.sh
+++ docker_scripts_ami/bash_scripts/start-nodejs.sh
@@ -1,1 +1,4 @@
-forever start -a --uid simple_nodejs_app /var/www/simple_nodejs_app/index.js
+#!/bin/sh
+set -e
+exec "$@"
+#forever start -a --uid simple_nodejs_app /var/www/simple_nodejs_app/index.js
