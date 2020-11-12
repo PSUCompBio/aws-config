ab7642dd568b7cfb693c016068e72ab58dae2bc2
Reuben Kraft <reuben.kraft@gmail.com>
2020-11-12 11:24:21 -0500

    Update start-nodejs.sh

--- docker_scripts_ami/bash_scripts/start-nodejs.sh
+++ docker_scripts_ami/bash_scripts/start-nodejs.sh
@@ -1,3 +1,10 @@
  #!/bin/sh
- forever -a --uid start /var/www/nsfcareer-api-service/server.js
- echo 'Success running the startup script for nsfcareer-api-service!
+cd /home/$UNAMEX
+source /home/$UNAMEX/.bash_profile
+##
+## npm will automatically pick up the server.js file in
+## nsfcareer-api-service
+npm run start --prefix /var/www/nsfcareer-api-service
+
+#forever -a --uid start /var/www/nsfcareer-api-service/server.js
+echo 'Success running the startup script for nsfcareer-api-service!'
