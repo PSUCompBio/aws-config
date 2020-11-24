5504fff2c72acbd091b927df4240ef6997b434c5
Reuben Kraft <reuben.kraft@gmail.com>
2020-11-24 14:21:34 -0500

    Update install-paraview.sh

--- docker_scripts_ami/bash_scripts/install-paraview.sh
+++ docker_scripts_ami/bash_scripts/install-paraview.sh
@@ -7,20 +7,23 @@ cd /home/$UNAMEX
 #export filename=ParaView-5.7.0-RC4-osmesa-MPI-Linux-Python3.7-64bit.tar.gz
 #wget --save-cookies cookies.txt 'https://docs.google.com/uc?export=download&id='$fileid -O-  | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1/p' > confirm.txt
 #wget --load-cookies cookies.txt -O $filename 'https://docs.google.com/uc?export=download&id='$fileid'&confirm='$(<confirm.txt)
-wget --no-check-certificate --no-proxy https://paraview-nsfcareer.s3.amazonaws.com/ParaView-5.7.0-RC4-osmesa-MPI-Linux-Python3.7-64bit.tar.gz
-gunzip ParaView-5.7.0-RC4-osmesa-MPI-Linux-Python3.7-64bit.tar.gz
-tar -xvf ParaView-5.7.0-RC4-osmesa-MPI-Linux-Python3.7-64bit.tar
-rm ParaView-5.7.0-RC4-osmesa-MPI-Linux-Python3.7-64bit.tar
-mv ParaView-5.7.0-RC4-osmesa-MPI-Linux-Python3.7-64bit Paraview-5.7.0
+#PARAVIEW_VERSION=ParaView-5.7.0-RC4-osmesa-MPI-Linux-Python3.7-64bit
+PARAVIEW_VERSION=ParaView-5.8.1-osmesa-MPI-Linux-Python3.7-64bit
+echo $PARAVIEW_VERSION
+wget --no-check-certificate --no-proxy https://paraview-nsfcareer.s3.amazonaws.com/$PARAVIEW_VERSION.tar.gz
+gunzip $PARAVIEW_VERSION.tar.gz
+tar -xvf $PARAVIEW_VERSION.tar
+rm $PARAVIEW_VERSION.tar
+#mv $PARAVIEW_VERSION Paraview
 #cd paraview
 #git submodule update --init --recursive
 #mkdir build
 #cd build
 echo "#LD_LIBRARY_PATH written from install-paraview.sh" >> /home/$UNAMEX/.bash_profile
-echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/home/$UNAMEX/Paraview-5.7.0/lib" >> /home/$UNAMEX/.bash_profile
+echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/home/$UNAMEX/$PARAVIEW_VERSION/lib" >> /home/$UNAMEX/.bash_profile
 #PATH=$PATH:/home/$UNAMEX/Paraview-5.7.0/bin
 echo "#PATH written from install-paraview.sh" >> /home/$UNAMEX/.bash_profile
-echo "export PATH=\$PATH:/home/$UNAMEX/Paraview-5.7.0/bin" >> /home/$UNAMEX/.bash_profile
+echo "export PATH=\$PATH:/home/$UNAMEX/$PARAVIEW_VERSION/bin" >> /home/$UNAMEX/.bash_profile
 echo " " >> /home/$UNAMEX/.bash_profile
 source /home/$UNAMEX/.bash_profile
 #cmake ../ -DCMAKE_BUILD_TYPE=Release -DPARAVIEW_ENABLE_PYTHON=ON -DVTK_USE_X=OFF -DVTK_OPENGL_HAS_OSMESA=ON -DPARAVIEW_BUILD_QT_GUI=OFF -DCMAKE_CXX_FLAGS="-lglut -lGLU"
