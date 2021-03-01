7796415a1be63a5e8a91bb755909b01129d35441
Reuben Kraft <reuben.kraft@gmail.com>
2021-03-01 10:04:29 -0500

    Update create_docker_image.sh

--- create_docker_image.sh
+++ create_docker_image.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
-INSTANCE="ubuntu"
+#INSTANCE_TYPE="ubuntu"
+INSTANCE_TYPE="ami"
 #
 
 echo "SCRIPT START TIME"
@@ -9,14 +10,27 @@ date
 echo " -------------------------------------------------------------------"
 echo " ----------                Instance Type                      ------"
 echo " -------------------------------------------------------------------"
-echo $INSTANCE
+echo $INSTANCE_TYPE
 echo " -------------------------------------------------------------------"
 echo " -------------------------------------------------------------------"
-if [ "$INSTANCE" == "ami" ]; then
+if [ "$INSTANCE_TYPE" == "ami" ]; then
         UNAMEX="ec2-user"
-elif [ "$INSTANCE" == "ubuntu" ]; then
+        HOME="/home/ec2-user"
+        nproc
+        NCPUS_VAR=$(nproc)
+        echo "ncpus = $NCPUS_VAR"
+        sudo yum update -y
+        sudo yum install docker -y
+        sudo yum install git -y
+        sudo service docker start
+        sudo usermod -a -G docker $UNAMEX
+elif [ "$INSTANCE_TYPE" == "ubuntu" ]; then
         UNAMEX="ubuntu"
         HOME="/home/ubuntu"
+        sudo apt-get update
+        sudo apt-get install docker.io -y
+        sudo systemctl start docker
+        sudo systemctl enable docker
 else
         echo "Instance Type does not exist in config_bash script - please update it."
 fi
@@ -28,15 +42,16 @@ echo " -------------------------------------------------------------------"
 echo " -------------------------------------------------------------------"
 cd /home/$UNAMEX
 
-sudo apt-get update
-sudo apt-get install docker.io -y
-systemctl start docker
-systemctl enable docker
+
 # clone specific branch
-#git clone -b minor-fixes https://github.com/PSUCompBio/aws-config.git
-# clone master branch
 git clone https://github.com/PSUCompBio/aws-config.git
-cd aws-config/docker_scripts
-docker build -t nsfcareer . 
+# clone master branch
+#git clone https://github.com/PSUCompBio/aws-config.git
+cd aws-config/docker_scripts_$INSTANCE_TYPE
+docker build \
+      --build-arg INSTANCE_TYPE=${INSTANCE_TYPE} \
+      --build-arg UNAMEX=${UNAMEX} \
+      --build-arg NCPUS_VAR=${NCPUS_VAR} \
+      -t nsfcareer.${INSTANCE_TYPE} .
 cd /home/$UNAMEX
-sudo chown -R ubuntu *
+sudo chown -R $UNAMEX  *
